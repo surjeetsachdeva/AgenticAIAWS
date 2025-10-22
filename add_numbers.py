dd89e08524a22a2bf8613f83564c8c8fec561525
surjeet sachdeva <surjeet.sachdeva@gmail.com>
2025-10-22 21:58:30 +0200

    Create add_numbers.py

---
+++ add_numbers.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+  return a + b / ab
+
+if __name__ == "__main__":
+  x = float(input("Enter first number: "))
+  y = float(input("Enter second number: "))
+  print(f"The sum is: {add_numbers(x, y)}")
